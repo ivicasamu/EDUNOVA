d51fe0d4c6e9b197b71702c057cfc6658076811f
Ivica Samu <ivicasamu@Ivicas-MacBook-Air.local>
2017-05-30 20:38:32 +0200

    no message

---
+++ izbornik.php
@@ -0,0 +1,47 @@
+<div class="row">
+
+	<div class="twelve columns header_nav" style="margin-bottom:0; box-shadow: none">
+
+		<div class="twelve columns">
+			<ul id="menu-header" class="nav-bar horizontal">
+
+				<li class="">
+					<a href="index.php">Početna</a>
+				</li>
+
+				<li class="has-flyout">
+					<a href="#">Primjer</a><a href="#" class="flyout-toggle"></a>
+
+					<ul class="flyout">
+
+						<li class="has-flyout">
+							<a href="primjer_jedan.php">Primjer 1</a>
+						</li>
+
+						<li class="has-flyout">
+							<a href="primjer_dva.php">Primjer 2</a>
+						</li>
+
+					</ul>
+				</li>
+
+				<li class="">
+					<a href="onama.php">O nama</a>
+				</li>
+
+				<li class="">
+					<a href="kontakt.php">Kontakt</a>
+				</li>
+
+			</ul>
+			<script type="text/javascript">
+				//<![CDATA[
+				$('ul#menu-header').nav - bar();
+				//]]>
+			</script>
+
+		</div>
+
+	</div>
+
+</div>
---
+++ zaglavlje.php
@@ -0,0 +1,5 @@
+<div class="row">
+
+	<a href="index.html"><img src="images/logo.png" alt="desc" style="border:none; margin-top:25px" /></a>
+
+</div>
