1659a9237bc601e6e4349ff7be961ee6a36d000f
Ivica Samu <>
2017-06-11 21:48:54 +0200

    dz4

---
+++ dz4/head.php
@@ -0,0 +1,13 @@
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width" />
+
+<title>Domaca zadaca 04</title>
+
+<link rel="stylesheet" href="stylesheets/foundation.min.css">
+
+<link rel="stylesheet" href="stylesheets/main.css">
+<link rel="stylesheet" href="stylesheets/app.css">
+
+<script src="javascripts/modernizr.foundation.js"></script>
+
+<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Playfair+Display:400italic' rel='stylesheet' type='text/css' />
---
+++ dz4/izbornik.php
@@ -0,0 +1,20 @@
+<div class="row">
+
+	<div class="twelve columns header_nav" style="margin-bottom:0; box-shadow: none">
+
+		<div class="twelve columns">
+			<ul id="menu-header" class="nav-bar horizontal">
+
+				<li class="right">
+					<a href="login.php">PRIJAVA</a>
+				</li>
+			</ul>
+			<script type="text/javascript">
+				//<![CDATA[
+				$('ul#menu-header').nav - bar();
+				//]]>
+			</script>
+
+		</div>
+	</div>
+</div>
