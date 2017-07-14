5b9816bff3fe38ad868fc72975a726566cfeac76
Ivica Šamu <ivica.samu@gmail.com>
2017-07-14 08:35:12 +0200

    Add files via upload
    Autorizacija korisnika iz baze podataka.

--- autorizacija.php
+++ autorizacija.php
@@ -4,11 +4,16 @@ if(!isset($_POST["korisnik"]) || !isset($_POST["korisnik"])){
 	header("location: " . $putanjAPP ."index.php");
 }
 
-if($_POST["korisnik"]==="dvd" && $_POST["lozinka"]==='123'){
-	$_SESSION["logiran"]=$_POST["korisnik"];
-	header("location: ". $putanjaAPP ."privatno/nadzornaPloca.php");
+$izraz=$veza->prepare("select * from operater where email=:email and lozinka=md5(:lozinka)");
+$izraz->execute(array("email"=>$_POST["korisnik"], "lozinka" =>$_POST["lozinka"]));
+$operater = $izraz->fetch(PDO::FETCH_OBJ);
+print_r($operater);
+
+if($operater!=null){
+	$_SESSION["logiran"] = $operater;
+	header("location: " . $putanjaAPP . "privatno/nadzornaPloca.php");
 }else{
-	header("location: ".putanjaAPP ."javno/prijava.php?neuspio&korisnik=". $_POST["korisnik"]);
+	header("location: " . $putanjaApp . "javno/prijava.php?neuspio&korisnik=" . $_POST["korisnik"]);
 }
 
 ?>
